d5255d465037dbdbb29e74c2126e680d38d26a84
Edgar de Graaff <edgraaff@jibes.nl>
2019-01-18 21:06:02 +0100

    support multiple proxies

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
-module.exports = {
+module.exports = [{
   rpcUrl: 'http://localhost:8545',
   port: 7545
-};
+}];
--- index.js
+++ index.js
@@ -4,37 +4,45 @@ const fetch = require('node-fetch');
 const BN = require('bn.js');
 const config = require('./config');
 
-const app = new Koa();
+for (const proxy of config) {
+  console.log(`starting proxy to ${proxy.rpcUrl} on port ${proxy.port}`);
 
-app.use(body({
-  limit: '10kb',
-  fallback: true
-}));
+  const app = new Koa();
 
-app.use(async ctx => {
-  if (ctx.request.body) {
-    const response = await fetch(config.rpcUrl, {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json'
-      },
-      body: JSON.stringify(ctx.request.body)
-    }).then(res => res.json());
+  app.use(body({
+    limit: '10kb',
+    fallback: true
+  }));
 
-    if (ctx.request.body.method === 'eth_getBlockByNumber' || ctx.request.body.method === 'eth_getBlockByHash') {
-      if (response && response.result && response.result.timestamp) {
-        const timestamp = new BN(response.result.timestamp.substring(2), 16);
-        // must be a timestamp in nanos if this is the case ...
-        if (timestamp.bitLength() > 53) {
-          // ... so convert it to seconds.
-          response.result.timestamp =
-            '0x' + timestamp.div(new BN(1000000000)).toString(16);
+  app.use(async ctx => {
+    if (ctx.request.body) {
+      console.log('request', ctx.request.body);
+
+      const response = await fetch(proxy.rpcUrl, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(ctx.request.body)
+      }).then(res => res.json());
+
+      console.log('response', response);
+
+      if (ctx.request.body.method === 'eth_getBlockByNumber' || ctx.request.body.method === 'eth_getBlockByHash') {
+        if (response && response.result && response.result.timestamp) {
+          const timestamp = new BN(response.result.timestamp.substring(2), 16);
+          // must be a timestamp in nanos if this is the case ...
+          if (timestamp.bitLength() > 53) {
+            // ... so convert it to seconds.
+            response.result.timestamp =
+              '0x' + timestamp.div(new BN(1000000000)).toString(16);
+          }
         }
       }
+
+      ctx.body = response;
     }
+  });
 
-    ctx.body = response;
-  }
-});
-
-app.listen(config.port);
+  app.listen(proxy.port);
+}
